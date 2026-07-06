2cb16c698718939ac9724aca852bc216df190bad
MichielVrins <michiel_678@hotmail.com>
2026-07-06 20:41:15 +0200

    Remove background from rating

--- src/app/_components/RatingBadge.tsx
+++ src/app/_components/RatingBadge.tsx
@@ -3,9 +3,5 @@ import type { MediaFragment } from "~/generated/graphql";
 export const RatingBadge: React.FC<{ media: MediaFragment }> = ({ media }) => {
   if (!media.averageScore) return null;
   const scaledRating = (media.averageScore / 10).toFixed(1);
-  return (
-    <div className="w-fit rounded bg-muted px-2">
-      <p>{scaledRating}</p>
-    </div>
-  );
+  return <span>{scaledRating}</span>;
 };
